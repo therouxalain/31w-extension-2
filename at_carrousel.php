9f99385642526309e3ebd486595722b6c07177b3
Alain Théroux <89165115+therouxalain@users.noreply.github.com>
2022-11-09 13:46:09 -0500

    Retour de congé de maladie - mise à jour

--- at_carrousel.php
+++ at_carrousel.php
@@ -1,30 +1,55 @@
 <?php
 
 /**
- * Plugin Name
- *
- * @package           Carrousel
- * @author            Alain Théroux
- * @copyright         Collège de Maisonneuve
- * @license           GPL-2.0-or-later
- *
- * @wordpress-plugin
- * Plugin Name:       at_carrousel
- * Description:       Description 
- * Author:            Alain Théroux
- * Author URI:        https://github.com/therouxalain
+ * @package igc31w
  */
 
-function genere_html(){
-    /////////////////////////////////////// HTML
+/**
+ * at_carrousel
+ * 
+ * @package              igc31w
+ * @author               Alain Thérouxx
+ * @copyright            2022 Alain Thérouxx
+ * @license              GPL v2 or later
+ * 
+ * @wordpress-plugin
+ * 
+ * Plugin Name:          at_carrousel
+ * Plugin URI:           https://github.com/RedouaneT/31w_extension
+ * Description:         Permet d'afficher une boite modale de l'image sélectionné d'une galerie
+ * Version:              1.0.0
+ * Requires at least:    5.2
+ * Requires PHP:         7.2
+ * Author:               Alain Thérouxx
+ * Author URI:           https://github.com/
+ * Text Domain:          31w_extension
+ * License:              GPL v2 or later
+ * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:
+ */
+
+
+function at_enqueue()
+{
+
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style('at_carrousel', plugin_dir_url(__FILE__) . "style.css", array(), $version_css, false);
+    wp_enqueue_script('at_carrousel_js', plugin_dir_url(__FILE__) . "js/carrousel.js", array(), $version_js, true);
+}
+
+add_action("wp_enqueue_scripts", "at_enqueue");
+
+function at_boite_carrousel()
+{
     // Le conteneur d'une boîte
-    $contenu = 
-    "<div class='boite'>"
-    . "<code>-------Auteur: " . get_the_author() . "</code>"
-    . "<date>-------Date de publication: " . get_the_date() . "</date>"
-    . "<h5>-------Adresse URL" . get_the_guid() . "</h5>"
-    . '</div> <!-- fin class="boite" -->';
-    
+    $contenu = "<section class='carrousel'>"
+        . '<button class="carrousel__x">X</button>'
+        . '<figure class="carrousel__figure"></figure>'
+        . '<form class="carrousel__form" action=""></form>'
+        . '</section> <!-- fin class="carrousel" -->';
+
     return $contenu;
-   }
-   add_shortcode('at_carrousel', 'genere_html');
+}
+add_shortcode('at_carrousel', 'at_boite_carrousel');
---
+++ readme.md
@@ -0,0 +1,4 @@
+# TP1
+## Création d'une extension et mise en ligne du site WP
+### Extension : Début du carrousel
+#### Auteur : Alain Théroux
