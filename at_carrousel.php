9e34ecee4260a14abfd5a9c892f8454766638af4
nom <Votre courriel>
2022-11-09 19:51:15 -0500

    Correction d'une erreur dans l'entête de mon plugin

--- at_carrousel.php
+++ at_carrousel.php
@@ -8,20 +8,20 @@
  * at_carrousel
  * 
  * @package              igc31w
- * @author               Alain Thérouxx
- * @copyright            2022 Alain Thérouxx
+ * @author               Alain Théroux
+ * @copyright            2022 Alain Théroux
  * @license              GPL v2 or later
  * 
  * @wordpress-plugin
  * 
  * Plugin Name:          at_carrousel
- * Plugin URI:           https://github.com/RedouaneT/31w_extension
+ * Plugin URI:           https://github.com/therouxalain/31w_alain/
  * Description:         Permet d'afficher une boite modale de l'image sélectionné d'une galerie
  * Version:              1.0.0
  * Requires at least:    5.2
  * Requires PHP:         7.2
- * Author:               Alain Thérouxx
- * Author URI:           https://github.com/
+ * Author:               Alain Théroux
+ * Author URI:           https://github.com/therouxalain/
  * Text Domain:          31w_extension
  * License:              GPL v2 or later
  * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
